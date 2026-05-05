5a6e99312f9e4e8abf07e189cf5523aa8a24dfae
wwknbh7xcb-commits <wwknbh7xcb@privaterelay.appleid.com>
2026-05-04 19:52:53 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/aswb-study-guide">
+    <BrowserRouter basename="/aswb-masters-exam">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
